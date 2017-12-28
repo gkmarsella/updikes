5bc3e0762f667661dad2d89fd12b91f0705791bd
Greg <gkmarsella@gmail.com>
2017-12-28 12:37:21 -0800

    added content box and styling

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_modus import Modus
 import os
 
 app = Flask(__name__)
+api = Modus(app)
+
+maps_key = app.config['GOOGLEMAPS'] = os.environ.get('GOOGLEMAPS')
 
 @app.route('/', methods=[ "GET"])
 def search():
-    return render_template("home.html")
+    return render_template("home.html", maps_key=maps_key)
 
 if os.environ.get('ENV') == 'production':
     debug = False
